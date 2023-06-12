be42e44218996e86847a456b9af670821fc9818a
toyinogunleye <olutoyinogun@gmail.com>
2023-06-12 23:43:42 +0100

    Building the search component with GeoDB API

---
+++ src/api.js
@@ -0,0 +1,10 @@
+// const url = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities";
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "424086460fmsh84e86af376e6f14p182f40jsn1aecbe1e0e5d",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
